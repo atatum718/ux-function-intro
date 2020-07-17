a2c9bd7151510fec5157138032d19f9bf6d10e83
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-07-17 12:49:50 -0400

    intro to functions

--- code.js
+++ code.js
@@ -9,6 +9,7 @@
 */
 function getNumberFive() {
   // Your code here
+  return 5;
 }
 
 /* 
@@ -28,6 +29,7 @@ console.log(getNumberFive() === 5);
 */
 function multiply(a, b) {
   // You code here
+  return a * b;
 }
 
 /* 
@@ -50,7 +52,7 @@ console.log(multiply(0, 0) === 0);
   Add the text to the end of the message, and then return the complete string.
 */
 function concatenate(message, text) {
-  // You code here
+  return message + text;
 }
 
 /* 
@@ -74,6 +76,12 @@ console.log(concatenate("", "New text!") === "New text!");
 */
 function absoluteValue(number) {
   // You code here
+
+  if (number > 0) {
+    return number;
+  } else {
+    return number * -1;
+  }
 }
 
 /* 
@@ -105,6 +113,15 @@ console.log(absoluteValue(0) === 0);
 */
 function evenOddOrZero(number) {
   // You code here
+  let result;
+  if (number === 0) {
+    result = "Zero";
+  } else if (number % 2 === 0) {
+    result = "Even";
+  } else {
+    result = "Odd";
+  }
+  return result;
 }
 
 /* 
